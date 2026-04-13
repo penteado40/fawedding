c856beb570443bcc6628127123f7be02f694d594
Felipe Penteado <flpenteado@gmail.com>
2026-04-12 21:05:24 -0300

    try update src

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/fawedding/" : "/",
+  base: "/",
 
   assetsInclude: ["**/*.PNG"],
   envPrefix: "VITE_URL_API",
