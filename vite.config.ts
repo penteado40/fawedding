d47f2f2b626190b972201480c98a214e8efc7510
Felipe Penteado <flpenteado@gmail.com>
2026-04-23 16:39:15 -0300

    update gifts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   base: "/",
 
   assetsInclude: ["**/*.PNG"],
-  envPrefix: "VITE_URL_API",
+  envPrefix: ["VITE_URL_API", "VITE_URL_GIFTS"],
   server: {
     host: "::",
     port: 8080,
